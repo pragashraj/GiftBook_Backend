49a33854bedf9d0615f176965d22539bcdd744d2
pragashraj <rajpragashraj30@gmail.com>
2021-10-10 16:07:54 +0530

    dummy description added

--- src/main/java/com/giftbook/giftBook/usecases/CreateNewPaymentUseCase.java
+++ src/main/java/com/giftbook/giftBook/usecases/CreateNewPaymentUseCase.java
@@ -120,7 +120,7 @@ public class CreateNewPaymentUseCase {
                 .createdAt(LocalDateTime.now())
                 .value(request.getValue())
                 .status("Active")
-                .description("")
+                .description("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
                 .payment(payment)
                 .user(user)
                 .build();
